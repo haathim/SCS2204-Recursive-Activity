9d2ce53a5683969069c403c818788046b4efe90d
haathimPC <haathimmunas@gmail.com>
2021-07-12 13:15:50 +0530

    Done all questions.

--- question-6.scala
+++ question-6.scala
@@ -5,10 +5,11 @@ object question6 extends App{
         case x => fib(x-1) + fib(x-2) 
     }
 
-    def printFib(n:Int):Unit = n match {
-        case 1 => println(fib(1))
-        case x => printFib(x-1); println(fib(x))
+    def printFib(n:Int):Unit = {
+        if(n>1) printFib(n-1)
+        println(fib(n))
     }
+    
 
     printFib(10)
 }
